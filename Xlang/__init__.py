1818b62108e067d69197637305835d5f5ba7d196
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-12-25 18:49:13 +0000

    Add documentation to __init__.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- Xlang/__init__.py
+++ Xlang/__init__.py
@@ -13,8 +13,22 @@ class XLang:
         self.root = self.tree.getroot()
     
     def get_meta(self):
+        """Get the meta tag from the XLang file.
+
+        Returns:
+            The meta tag from the XLang file.
+        """
         return self.root.find('meta')
     
     def get_string(self, name, lang):
+        """Get a string from the XLang file.
+        
+        Args:
+            name: The name of the string to get.
+            lang: The language of the string to get.
+        
+        Returns:
+            The string from the XLang file.
+        """
         return self.root.find('strings').find('string[@name="'+name+'"]').find(lang).text
 
